8448d8179eed0b3d3de9d8030fa7b48fb0859bee
thedemonsid <siddheshshrirame@gmail.com>
2025-02-06 21:16:45 +0530

    enhance breadcrumb navigation to dynamically display path items

--- components/breadcrumb.tsx
+++ components/breadcrumb.tsx
@@ -12,17 +12,34 @@ import {
 } from "./ui/breadcrumb";
 
 const Navigator = () => {
-  const pathname = usePathname();
+  const pathname = usePathname(); // returns {/dashboard/xyz}
+  // function to extract all the pathnames
+  const path = pathname.split("/").filter(Boolean);
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem className="hidden md:block">
-          <BreadcrumbLink href="#">{pathname}</BreadcrumbLink>
+          <BreadcrumbLink href="/dashboard">Building Your Project</BreadcrumbLink>
         </BreadcrumbItem>
-        <BreadcrumbSeparator className="hidden md:block" />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-        </BreadcrumbItem>
+        {path.map((item, index) => {
+          const isLast = index === path.length - 1;
+          return (
+            <div key={index} className="flex items-center">
+              <BreadcrumbSeparator className="hidden md:block" />
+              {isLast ? (
+                <BreadcrumbPage key={index} className="hidden md:block">
+                  {item.toUpperCase()}
+                </BreadcrumbPage>
+              ) : (
+                <BreadcrumbItem key={index} className="hidden md:block">
+                  <BreadcrumbLink href={`/${item}`}>
+                    {item.toUpperCase()}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              )}
+            </div>
+          );
+        })}
       </BreadcrumbList>
     </Breadcrumb>
   );
