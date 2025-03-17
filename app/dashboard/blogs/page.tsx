4eb8e99d8254220d63f1f9fa7397c339e39c51ac
thedemonsid <siddheshshrirame@gmail.com>
2025-03-17 15:59:52 +0530

    feat: implement BlogsComponent and BlogCard for enhanced blog display

--- app/dashboard/blogs/page.tsx
+++ app/dashboard/blogs/page.tsx
@@ -1,7 +1,8 @@
+import BlogsComponent from "@/components/blogs/blogs-main";
 import React from "react";
 
 const Blogs = () => {
-  return <main className="flex-1 p-6 bg-white ">Blogs Blogs</main>;
+  return <BlogsComponent></BlogsComponent>;
 };
 
 export default Blogs;
