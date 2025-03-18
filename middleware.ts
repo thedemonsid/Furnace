f64fbbb1b7bf192b45967201e6b3701d694f819e
thedemonsid <siddheshshrirame@gmail.com>
2025-03-18 18:09:06 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -23,5 +23,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/projects"], // Apply middleware to specific routes
+  matcher: ["/sdfghjk"], // Apply middleware to specific routes
 };
