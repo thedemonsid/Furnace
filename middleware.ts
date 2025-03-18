b573a4e0c2467ee8ae6b5c6507d02fc58b8bf5f3
thedemonsid <siddheshshrirame@gmail.com>
2025-03-18 18:08:56 +0530

    added middleware logic

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+export async function middleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+      },
+    }
+  );
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/projects"], // Apply middleware to specific routes
+};
